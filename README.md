e65479548f0dbbe95a11b040abcd7df1e7de3988
Jörg Bleyel <jbleyel@gmx.net>
2021-05-31 22:56:17 +0200

    add e2isubparser

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Allows to list directory
 
 # cmdwrap
 Command line args splitter
+
+# e2isubparser
+Python module for text subtitles parsing
---
+++ e2isubparser/src/html/include/htmlcleaner.h
@@ -0,0 +1,8 @@
+#ifndef _HTML_CLEANER_H_
+#define _HTML_CLEANER_H_
+
+#include <stdio.h>
+
+char * strip_html_tags(const char *in, int isize, int *osize);
+
+#endif
